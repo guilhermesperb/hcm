f6c8b57f55f56a722943dd32f76692adfa272598
Guilherme Filipi Sperb <guilhermesperb@GuilhermeMac.local>
2022-06-27 01:31:20 -0300

    prova

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # HCM
 
 ## Solução
-Aplicação desenvolvida para servir como gateway entre as aplicações frontend de marcação de ponto e o sistema legado da companhia.
+Esta aplicação foi desenvolvida para servir como gateway entre as aplicações frontend de marcação de ponto e o sistema legado da companhia.
 
-Através de uma API em NodeJS, é possível tratar milhares de requisições com um tempo de resposta muito menor do que o sistema legado é capaz de responder. 
+Através desta API em NodeJS, é possível tratar milhares de requisições com um tempo de resposta muito menor do que o sistema legado é capaz de responder.
 
-Juntamente com a fila criada com o Apache Kafka, todas as requisições são recebidas e enviadas conforme a disponibilidade para a API legada, sem atrasos de resposta para o usuário, que terá a confirmação logo após o recebimento da primeira API.
+A fila criada com o Apache Kafka recebe todas as requisições e as envia conforme a disponibilidade para a API legada, sem atrasos de resposta para o usuário, que terá a confirmação logo após o recebimento da primeira API. Assim, é possível otimizar ainda mais a rotina da companhia que contratou esta soluçào.
  
 
 ## Execução
@@ -23,6 +23,7 @@ Um exemplo pode ser acessado [aqui](http://hcm.guilhermesperb.com.br)
 sequenceDiagram
 Navegador/App/IoT ->> API: Envio do registro de marcação do ponto
 API ->> Kafka: Envia registro para fila
+API ->> Navegador/App/IoT: Confirma o recebimento
 Kafka ->> Consumer: Envia registros pendentes  para Sistema Legado
 Consumer ->> Navegador/App/IoT: Notifica sobre a gravação
 ```
--- frontend/index.js
+++ frontend/index.js
@@ -1,40 +1,73 @@
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Create the XHR object.
-function createCORSRequest(method, url) {
+function checkMaxRequests(){
+    countAPI++;
+
+    if (countAPI >= maxRequests){
+        clearInterval(apiInterval);
+        const finalTime = new Date()
+        document.getElementById('timeAPI').innerHTML = `${(finalTime.getTime() - initialTime.getTime())/1000} segundos`;
+    } 
+}
+
+function checkMaxRequestsLegacy(){
+    countLegacy++;
+
+    if (countLegacy >= maxRequests){
+        clearInterval(apiIntervalLegacy);
+        const finalTime = new Date()
+        document.getElementById('timeLegacy').innerHTML = `${(finalTime.getTime() - initialTimeLegacy.getTime())/1000} segundos`;
+    } 
+}
+
+
+function createRequest(method, url) {
     var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader("Accept", "application/json");
     return xhr;
 }
 
 function sendClockingRegister(data){
-    const xhr = createCORSRequest('POST', 'http://localhost:3001/clocking-register')
+    const xhr = createRequest('POST', 'http://localhost:3001/clocking-register')
 
     xhr.onload = () => {
         if (xhr.status >= 200 && xhr.status < 300) {
             const response = JSON.parse(xhr.responseText);
             document.getElementById('registers').innerHTML = JSON.stringify(data) + '<br>' + document.getElementById('registers').innerHTML
+            checkMaxRequests();
             return;
         }
     };
 
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader("Accept", "application/json");
     xhr.send(JSON.stringify(data));
 }
 
-setInterval(() => {
+function sendClockingRegisterLegacy(data){
+    fetch("https://api.mockytonk.com/proxy/ab2198a3-cafd-49d5-8ace-baac64e72222", {
+        method: "POST",
+        headers: [
+            ["Content-Type", "application/json"]
+        ],
+        mode: 'no-cors',
+        body: JSON.stringify(JSON.stringify(data))
+    }).then(resp=>{
+        document.getElementById('registersLegacy').innerHTML = JSON.stringify(data) + '<br>' + document.getElementById('registersLegacy').innerHTML
+        checkMaxRequestsLegacy();
+    })
+}
+
+const maxRequests = 15;
+var countAPI = 0;
+var countLegacy = 0;
+const initialTime = new Date()
+
+var apiInterval = setInterval(() => {
     const employerId = getRandomIntInclusive(1,10);
     const employeeId = getRandomIntInclusive(1,999);
     const includeAt = new Date();
@@ -45,3 +78,17 @@ setInterval(() => {
         employerId
     })
 }, 100)
+
+const initialTimeLegacy = new Date()
+
+var apiIntervalLegacy = setInterval(() => {
+    const employerId = getRandomIntInclusive(1,10);
+    const employeeId = getRandomIntInclusive(1,999);
+    const includeAt = new Date();
+    
+    sendClockingRegisterLegacy({
+        includeAt,
+        employeeId,
+        employerId
+    })
+}, 500)
